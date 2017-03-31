7b1c91c4b47881a44e606d20f82282db917550a1
Edward Goo <Edwardx89@yahoo.com>
2017-03-31 12:36:49 -0400

    added middleware and installed thunk

--- browser/react/store.js
+++ browser/react/store.js
@@ -1,6 +1,10 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducers/root-reducer';
+import {createLogger} from 'redux-logger';
 
 
+const logger = createLogger();
+const Middleware = applyMiddleware(logger);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default createStore(reducer, composeEnhancers(Middleware));
