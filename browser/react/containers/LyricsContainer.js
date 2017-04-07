f71b6a8d47ced293b9f6e34b2d649c717f8e8f2c
Pimpila <pimpila@gmail.com>
2017-04-07 13:18:48 -0400

    added comments

--- browser/react/containers/LyricsContainer.js
+++ browser/react/containers/LyricsContainer.js
@@ -10,6 +10,9 @@ export default class extends Component {
 
     super();
 
+    // local state made up of user input and all of the store.
+    // b/c we subscribe below just the lyrics piece will update every time the store updates.
+
     this.state = Object.assign({
       artistQuery: '',
       songQuery: ''
@@ -22,12 +25,17 @@ export default class extends Component {
 
   // once component mounts, update local state with store state
   componentDidMount() {
-    // set subscribe to a reference on the component so you can call it later (in componentWillUnmount)
+    // create a reference to store.subscribe within the component
+    // so you can call it later (in componentWillUnmount)
+    // here's we're pulling off just the .lyrics part of global state.
     this.unsubscribe = store.subscribe(() => {
-      this.setState({lyrics: store.getState().lyrics}); // pull off just the .lyrics part of global state.
+      this.setState({lyrics: store.getState().lyrics});
     });
   }
-  // before component unmounts, unsubscribe. this turns off the listening we activated in subsbribe, so when global state changes, we're no longer trying to update our LyricsContainer's local state because our view is no longer mounted.
+  // before component unmounts, unsubscribe. this is possible b/c store.subscribe is
+  // wired to also return a function that will turn the subsscribing off. so when global
+  // state changes, we're no longer trying to update our LyricsContainer's local state
+  // because our view is no longer mounted.
   componentWillUnmount() {
     this.unsubscribe();
   }
@@ -44,7 +52,8 @@ export default class extends Component {
 
   handleSubmit(event) {
     event.preventDefault()
-    //this submit event will dispatch the selected artist and song to the middleware action creator (fetchLyrics).
+    // our event handler dispatches the selected artist and song to
+    // the middleware action creator (fetchLyrics).
     if (this.state.artistQuery && this.state.songQuery) {
       store.dispatch(fetchLyrics(this.state.artistQuery, this.state.songQuery));
     }
@@ -62,3 +71,6 @@ export default class extends Component {
   }
 
 }
+
+
+
